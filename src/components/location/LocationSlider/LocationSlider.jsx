95f0dca834a35ae9a2cfc330704d52f45facf489
bshepeliuk <bogdan__sh@ukr.net>
2023-05-15 21:40:28 +0300

    change location on swipe

--- src/components/location/LocationSlider/LocationSlider.jsx
+++ src/components/location/LocationSlider/LocationSlider.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper, useSwiperSlide } from 'swiper/react';
 import 'swiper/css/navigation';
 import 'swiper/css';
 
 import styles from './LocationSlider.module.sass';
 import LocationItem from '../LocationItem/LocationItem';
-import { geocoder } from '@/helpers';
+import { geocoder, getLatLngByPlace } from '@/helpers';
 import getLocationsSortedByDistance from '@/helpers/getLocationsSortedByDistance';
 
 const LocationSlider = ({ locations, map, onSelect }) => {
@@ -32,10 +32,25 @@ const LocationSlider = ({ locations, map, onSelect }) => {
     }
   };
 
+  const onSlideChange = async (params) => {
+    const point = await geocoder({ address: sortedLocations[params.activeIndex].address });
+    const coordinates = getLatLngByPlace(point.results[0]);
+
+    map.panTo(coordinates);
+    map.setZoom(11);
+  };
+
   const sortedLocations = getLocationsSortedByDistance({ locations });
 
   return (
-    <Swiper className={styles.slider} slidesPerView="auto" spaceBetween={16}>
+    <Swiper
+      className={styles.slider}
+      slidesPerView="auto"
+      centeredSlides
+      centeredSlidesBounds
+      spaceBetween={16}
+      onSlideChange={onSlideChange}
+    >
       {sortedLocations.map((location) => {
         return (
           <SwiperSlide key={location.id} className={styles.slide}>
@@ -54,5 +69,29 @@ const LocationSlider = ({ locations, map, onSelect }) => {
     </Swiper>
   );
 };
+
+function Slide({
+  hasBookFreeBtn,
+  location,
+  className,
+  detailsContainerClassName,
+  onLocationClick,
+  selectedId,
+  distance,
+}) {
+  const swiperSlide = useSwiperSlide();
+  console.log({ swiperSlide });
+  return (
+    <LocationItem
+      hasBookFreeBtn={hasBookFreeBtn}
+      location={location}
+      className={styles.locationItem}
+      detailsContainerClassName={styles.detailsContainer}
+      onLocationClick={onLocationClick}
+      selectedId={selectedId}
+      distance={distance}
+    />
+  );
+}
 
 export default LocationSlider;
